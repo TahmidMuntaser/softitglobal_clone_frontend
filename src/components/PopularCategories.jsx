ae184a6ff8756da9fdf673f79da4eb6930a7fbd0
Tahmid <tahmid25muntaser@gmail.com>
2026-06-04 15:52:48 +0600

    FEAT: api fetch PopularCategories component

--- src/components/PopularCategories.jsx
+++ src/components/PopularCategories.jsx
@@ -1,21 +1,37 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-const categories = [
-  { name: "Honey", slug: "honey", img: "/images/logo.webp" },
-  { name: "Spices", slug: "spices", img: "/images/logo.webp" },
-  { name: "Frozen Snacks", slug: "frozen-snacks", img: "/images/logo.webp" },
-  { name: "Tea", slug: "tea", img: "/images/logo.webp" },
-  { name: "Rice", slug: "rice", img: "/images/logo.webp" },
-  { name: "Oil", slug: "oil", img: "/images/logo.webp" },
-  
-];
 
 export default function PopularCategories() {
-  
   const scrollRef = useRef(null);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/categories/`
+        );
+
+        const data = await res.json();
+
+        const popular = (data.results || []).filter(
+          (item) => item.is_popular === true
+        );
+
+        setCategories(popular);
+      } 
+      
+      catch (err) {
+        console.error("Failed to fetch categories", err);
+      }
+    };
+
+    fetchCategories();
+
+  }, []);
 
   const scroll = (dir) => {
     if (!scrollRef.current) return;
@@ -28,7 +44,8 @@ export default function PopularCategories() {
 
   return (
     <section className="container-fluid popular-categories-section">
-      
+     
+      {/* header  */}
       <div className="category-title-row">
         <b />
         <span>Popular Categories</span>
@@ -36,17 +53,25 @@ export default function PopularCategories() {
       </div>
 
       <div className="scroll-wrapper">
+        
         <button className="arrow left" onClick={() => scroll("left")}>
           <FaChevronLeft />
         </button>
 
+        {/* category  */}
         <div className="category-scroll" ref={scrollRef}>
+          
           {categories.map((c) => (
-            <a key={c.slug} href={`/shop/${c.slug}`} className="category-card">
-              <img src={c.img} alt={c.name} className="category-image" />
+            <a key={c.id} href={`/shop/${c.slug}`} className="category-card">
+              <img
+                src={c.image_url}
+                alt={c.name}
+                className="category-image"
+              />
               <span className="ctgName">{c.name}</span>
             </a>
           ))}
+
         </div>
 
         <button className="arrow right" onClick={() => scroll("right")}>
